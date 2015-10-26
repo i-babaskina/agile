ab40f70bd698e447bab9ad6c7316c564a8b7c772
Artem Minakov <frozermag@gmail.com>
2015-10-27 00:34:03 +0200

    Add wordsActivity

--- app/src/main/java/com/example/babaskina/alias/MainActivity.java
+++ app/src/main/java/com/example/babaskina/alias/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.babaskina.alias;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Word> mWords;
+
+    public class WordAdapter extends ArrayAdapter<Word> {
+        Context mContext;
+
+        public WordAdapter(ArrayList<Word> tasks) {
+            super(getApplicationContext(), 0, tasks);
+            this.mContext = getApplicationContext();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.list_item_words, null);
+            }
+
+            Word task = getItem(position);
+
+            TextView titleTextView = (TextView) convertView.findViewById(R.id.word_list_item_titleTextView);
+            titleTextView.setText(task.getTitleWord());
+
+            return convertView;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView lvMain = (ListView) findViewById(R.id.listViewTasksAddTimetableOnData);
+        mWords = WordLab.get(this).getWords();
+        WordAdapter adapter = new WordAdapter(mWords);
+        lvMain.setAdapter(adapter);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/babaskina/alias/Word.java
@@ -0,0 +1,34 @@
+package com.example.babaskina.alias;
+
+import java.util.UUID;
+
+/**
+ * Created by artem on 26.10.15.
+ */
+public class Word {
+
+    private String titleWord;
+    private UUID id;
+
+    public Word(){
+        id = UUID.randomUUID();
+        titleWord = "word";
+    }
+
+    public Word(String title){
+        id = UUID.randomUUID();
+        titleWord = title;
+    }
+
+    public String getId() {
+        return id.toString();
+    }
+
+    public String getTitleWord() {
+        return titleWord;
+    }
+
+    public void setTitleWord(String titleWord) {
+        this.titleWord = titleWord;
+    }
+}
