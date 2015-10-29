2b4200d0f633cf5f96e875e3f1ea0980d98b0e04
Andriy Briznichenko <Andriy Briznichenko>
2015-10-29 16:12:58 +0200

    test commit

---
+++ app/src/main/java/com/example/babaskina/alias/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.babaskina.alias;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
