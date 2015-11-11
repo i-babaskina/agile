ea4b89bd4fe06e95c3bcebaba08833c226144b64
Artem Minakov <frozermag@gmail.com>
2015-11-11 21:16:46 +0200

    Implementation adding dictionary

--- app/src/main/java/com/example/babaskina/alias/DictionariesActivity.java
+++ app/src/main/java/com/example/babaskina/alias/DictionariesActivity.java
@@ -1,11 +1,13 @@
 package com.example.babaskina.alias;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +23,14 @@ import java.util.ArrayList;
  */
 public class DictionariesActivity extends AppCompatActivity {
 
+    private static final String TABLE_DICTIONARY = "dictionaries";
+    private static final String COLUMN_TASK_TITLE = "title";
+
+    private String dictTitle;
+
     private ArrayList<Dictionary> mDictionaries;
+
+    private AliasDatabaseHelper aliasDBHelper;
 
     public class DictionariesAdapter extends ArrayAdapter<Dictionary> {
         Context mContext;
@@ -50,6 +59,9 @@ public class DictionariesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionaries);
+        DictionaryLab.get(this).clear();
+
+        queryDictDBHelper();
 
         final ListView lvMain = (ListView) findViewById(R.id.listViewDictionariesActivity);
         mDictionaries = DictionaryLab.get(this).getDictionary();
@@ -67,6 +79,28 @@ public class DictionariesActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        DictionaryLab.get(this).clear();
+        queryDictDBHelper();
+        ListView listView = (ListView) this.findViewById(R.id.listViewDictionariesActivity);
+        DictionariesAdapter adapter = new DictionariesAdapter(mDictionaries);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        DictionaryLab.get(this).clear();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        DictionaryLab.get(this).clear();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -80,13 +114,51 @@ public class DictionariesActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                Intent i = new Intent(getApplicationContext(), AddElementsActivity.class);
+                startActivityForResult(i, 0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {
+            return;
+        }
+        dictTitle = data.getStringExtra("title");
+        Dictionary dictionary = new Dictionary(dictTitle);
+        addDictToDatabase(dictionary);
+    }
+
+    private void queryDictDBHelper() {
+        aliasDBHelper = new AliasDatabaseHelper(getApplicationContext());
+        SQLiteDatabase db = aliasDBHelper.getWritableDatabase();
+
+        Cursor c = db.query(TABLE_DICTIONARY, null, null, null, null, null, null);
+
+        if (c.moveToFirst()) {
+
+            int titleColIndex = c.getColumnIndex(COLUMN_TASK_TITLE);
+
+
+            do {
+                Dictionary resDict = new Dictionary();
+                resDict.setTitleDictionary(c.getString(titleColIndex));
+                DictionaryLab.get(this).addDictionary(resDict);
+            } while (c.moveToNext());
+        }
+        c.close();
+    }
+
+    private void addDictToDatabase(Dictionary dictionary) {
+        ContentValues cv = new ContentValues();
+        SQLiteDatabase db = aliasDBHelper.getWritableDatabase();
+        cv.put(COLUMN_TASK_TITLE, dictionary.getTitleDictionary());
+        db.insert(TABLE_DICTIONARY, null, cv);
     }
 }
